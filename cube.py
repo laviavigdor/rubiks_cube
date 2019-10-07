865f1f045410eb2ad9dd6d8f5d08e03c57616f26
Lavi Avigdor <lavi.avigdor@toluna.com>
2019-10-08 01:19:38 +0300

    progress

--- cube.py
+++ cube.py
@@ -2,9 +2,15 @@
 # Notation: https://ruwix.com/the-rubiks-cube/notation/advanced/
 import re
 from collections import Counter
-from colorama import Fore, Back, Style
+from colorama import Back, Style
+import copy
 
-PERFECT_CUBE = [[color] * 9 for color in 'YBRGOW']
+
+POSSIBLE_MOVES = ["B", "Bi", "B2", "U", "Ui", "U2", "D", "Di", "D2", "L", "Li", "L2", "F", "Fi", "F2", "R", "Ri", "R2"]
+UNDO_MOVE = {"B":"Bi", "Bi":"B", "B2":"B2", "U":"Ui", "Ui":"U", "U2":"U2", "D":"Di", "Di":"D", "D2":"D2", "L":"Li", "Li":"L", "L2":"L2", "F":"Fi", "Fi":"F", "F2":"F2", "R":"Ri", "Ri":"R", "R2":"R2"}
+SUPERFLIP_MOVES = "UR2FBRB2RU2LB2RU'D'R2FR'LB2U2F2"
+COLORS = ['Y', 'B', 'R', 'G', 'O', 'W']
+PERFECT_CUBE = [[color] * 9 for color in COLORS]
 COLOR_MAP = {
     'G': Back.GREEN,
     'Y': Back.YELLOW,
@@ -14,12 +20,12 @@ COLOR_MAP = {
     'B': Back.BLUE,
 }
 
+
 class Cube:
     def __init__(self):
         self.data = PERFECT_CUBE
 
-    r = lambda self, f: [f[a + b] for a in (0, 1, 2) for b in
-                         (6, 3, 0)]  # only rotates the stickers on a face, clockwise a quarter-turn
+    r = lambda self, f: [f[a + b] for a in (0, 1, 2) for b in (6, 3, 0)]  # only rotates the stickers on a face, clockwise a quarter-turn
     U = lambda self, c: [self.r(c[0])] + [c[j + 1][:3] + c[j or 4][3:] for j in (1, 2, 3, 0)] + [c[5]]
     y = lambda self, c: [self.r(c[0])] + c[2:5] + [c[1], self.r(self.r(self.r(c[5])))]
     z = lambda self, c: [c[2], self.r(self.r(self.r(c[1]))), c[5], self.r(c[3]), c[0][::-1], c[4][::-1]]
@@ -45,12 +51,12 @@ class Cube:
 
         rows = [
             [
-                nine_spaces,nine_spaces,
+                nine_spaces, nine_spaces,
                 self.data[0]
             ],
             self.data[1:5],
             [
-                nine_spaces,nine_spaces,
+                nine_spaces, nine_spaces,
                 self.data[5]
             ]
         ]
@@ -63,11 +69,31 @@ class Cube:
             ) for row in rows
         )
 
-        for letter in COLOR_MAP:
-            color = COLOR_MAP[letter]
-            output = output.replace(letter, color + '  ' + Style.RESET_ALL)
+        output = self.colorize_text(output)
 
         return output
+
+    def colorize_text(self, output):
+        for letter in COLOR_MAP:
+            color = COLOR_MAP[letter]
+            output = output.replace(letter, color + letter + letter + Style.RESET_ALL)
+        return output
+
+    def get_slim_representation(self):
+        data = copy.deepcopy(self.data)
+
+        for face in data:
+            del face[4] # Remove center, which is constant
+
+        flattened = ''.join([color for face in data for color in face])
+
+        # flattened = self.colorize_text(flattened)
+
+        for index in range(len(COLORS)):
+            letter = COLORS[index]
+            flattened = flattened.replace(letter, str(index))
+
+        return flattened
 
     def grade(self):
         '''
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from cube import Cube
+from cube import Cube, POSSIBLE_MOVES, UNDO_MOVE
+from random import randrange, choice, seed
+seed( 30 )
 
 
 def main():
+    cube = Cube()
 
-    inputs = ["superfl!p: UR2FBRB2RU2LB2RU'D'R2FR'LB2U2F2", "", "U", "Ui", "U2", "L'", "R", "U2 L' D"]
-    #inputs = [""]
-    for input in inputs:
-        cube = Cube()
-        cube.rotate(input)
-        print('grade',cube.grade())
-        print(input)
+    moves = [choice(POSSIBLE_MOVES) for _ in range(10)]
+    for move in moves:
+        cube.rotate(move)
+        print(cube.get_slim_representation())
         print(cube)
+    #     print(' ')
+    #     # print(cube.grade())
+
+    # print(cube)
+    # print('---')
+    # for move in [UNDO_MOVE[move] for move in list(reversed(moves))]:
+    #     cube.rotate(move)
+    #     # print(cube.grade())
+    #
+    # print(cube)
+    # print(moves)
+    # for input in inputs:
+    #     cube = Cube()
+    #     cube.rotate(input)
+    #     print('grade',cube.grade())
+    #     print(input)
+    #     print(cube)
 
 
 if __name__ == '__main__':
